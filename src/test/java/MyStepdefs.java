acf3328419eafec6366a118ab7ab48a6920695fb
franksha <sharon.frank@microfocus.com>
2020-01-07 15:03:41 +0200

    initial

---
+++ src/test/java/MyStepdefs.java
@@ -0,0 +1,26 @@
+import cucumber.api.java.en.And;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class MyStepdefs {
+    @Given("^given$")
+    public void given() {
+    }
+
+    @When("^when$")
+    public void when() {
+    }
+
+    @Then("^then$")
+    public void then() {
+    }
+
+    @And("and")
+    public void and() {
+    }
+
+    @And("and{int}")
+    public void and(int arg0) {
+    }
+}
---
+++ src/test/resources/my.feature
@@ -0,0 +1,7 @@
+Feature: my
+  Scenario: a
+    Given given
+    When when
+    Then then
+    And and
+    And and2
